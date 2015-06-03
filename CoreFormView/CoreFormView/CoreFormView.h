e4f3b61e4db1149efe7adffb09b3da94ff1aa231
Charlin_冯成林 <1761904945@qq.com>
2015-06-03 13:18:15 +0800

    CoreFormView
    表格

---
+++ CoreFormView/CoreFormView/CoreFormView.h
@@ -0,0 +1,13 @@
+//
+//  CoreFormView.h
+//  CoreFormView
+//
+//  Created by 冯成林 on 15/6/3.
+//  Copyright (c) 2015年 冯成林. All rights reserved.
+//
+
+#import "KWFormView.h"
+
+@interface CoreFormView : KWFormView
+
+@end
---
+++ CoreFormView/CoreFormView/CoreFormView.m
@@ -0,0 +1,21 @@
+//
+//  CoreFormView.m
+//  CoreFormView
+//
+//  Created by 冯成林 on 15/6/3.
+//  Copyright (c) 2015年 冯成林. All rights reserved.
+//
+
+#import "CoreFormView.h"
+
+@implementation CoreFormView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
